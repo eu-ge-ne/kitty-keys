a8b71bf33fe99f89b3e41570052ffc4512450f2c
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-16 00:22:40 -0400

    fix

--- src/parse.ts
+++ src/parse.ts
@@ -7,6 +7,89 @@ const legacy_csi1_re = /(1;(?<modifier>.*))?(?<key>[ABCDEFHPQS])/s;
 // SS3 {ABCDEFHPQRS}
 const legacy_ss3_re = /(?<key>[ABCDEFHPQRS])/s;
 
+const map = new Map<string, Key[]>([
+  ["\x0d", [
+    new FuncKey("ENTER"),
+    new FuncKey("ENTER", { ctrl: true }),
+    new FuncKey("ENTER", { shift: true }),
+    new FuncKey("ENTER", { ctrl: true, shift: true }),
+  ]],
+  ["\x1b\x0d", [
+    new FuncKey("ENTER", { alt: true }),
+    new FuncKey("ENTER", { alt: true, shift: true }),
+    new FuncKey("ENTER", { ctrl: true, alt: true }),
+  ]],
+  ["\x1b", [
+    new FuncKey("ESC"),
+    new FuncKey("ESC", { ctrl: true }),
+    new FuncKey("ESC", { shift: true }),
+    new FuncKey("ESC", { ctrl: true, shift: true }),
+    new FuncKey("3", { ctrl: true }),
+  ]],
+  ["\x1b\x1b", [
+    new FuncKey("ESC", { alt: true }),
+    new FuncKey("ESC", { alt: true, shift: true }),
+    new FuncKey("ESC", { ctrl: true, alt: true }),
+  ]],
+  ["\x7f", [
+    new FuncKey("BACKSPACE"),
+    new FuncKey("BACKSPACE", { shift: true }),
+  ]],
+  ["\x08", [
+    new FuncKey("BACKSPACE", { ctrl: true }),
+    new FuncKey("BACKSPACE", { ctrl: true, shift: true }),
+  ]],
+  ["\x1b\x7f", [
+    new FuncKey("BACKSPACE", { alt: true }),
+    new FuncKey("BACKSPACE", { alt: true, shift: true }),
+  ]],
+  ["\x1b\x08", [
+    new FuncKey("BACKSPACE", { ctrl: true, alt: true }),
+  ]],
+  ["\x09", [
+    new FuncKey("TAB"),
+    new FuncKey("TAB", { ctrl: true }),
+  ]],
+  ["\x1b\x09", [
+    new FuncKey("TAB", { alt: true }),
+    new FuncKey("TAB", { ctrl: true, alt: true }),
+  ]],
+  ["\x1b[Z", [
+    new FuncKey("TAB", { shift: true }),
+    new FuncKey("TAB", { ctrl: true, shift: true }),
+  ]],
+  ["\x1b\x1b[Z", [
+    new FuncKey("TAB", { alt: true, shift: true }),
+  ]],
+  ["\x20", [
+    new CharKey(" "),
+    new CharKey(" ", { shift: true }),
+  ]],
+  ["\x00", [
+    new CharKey(" ", { ctrl: true }),
+    new CharKey(" ", { ctrl: true, shift: true }),
+    new CharKey("2", { ctrl: true }),
+  ]],
+  ["\x1b\x20", [
+    new CharKey(" ", { alt: true }),
+    new CharKey(" ", { alt: true, shift: true }),
+  ]],
+  ["\x1b\x00", [
+    new CharKey(" ", { ctrl: true, alt: true }),
+  ]],
+  ["\x1f", [
+    new CharKey("/", { ctrl: true }),
+  ]],
+  ["0", [
+    new CharKey("0"),
+    new CharKey("0", { ctrl: true }),
+  ]],
+  ["1", [
+    new CharKey("1"),
+    new CharKey("1", { ctrl: true }),
+  ]],
+]);
+
 const decoder = new TextDecoder();
 
 /**
@@ -15,126 +98,13 @@ const decoder = new TextDecoder();
 export function parse(buf: Uint8Array): Key[] {
   const text = decoder.decode(buf);
 
-  if (text === "\x0d") {
-    return [
-      new FuncKey("ENTER"),
-      new FuncKey("ENTER", { ctrl: true }),
-      new FuncKey("ENTER", { shift: true }),
-      new FuncKey("ENTER", { ctrl: true, shift: true }),
-    ];
-  }
-  if (text === "\x1b\x0d") {
-    return [
-      new FuncKey("ENTER", { alt: true }),
-      new FuncKey("ENTER", { alt: true, shift: true }),
-      new FuncKey("ENTER", { ctrl: true, alt: true }),
-    ];
-  }
-
-  if (text === "\x1b") {
-    return [
-      new FuncKey("ESC"),
-      new FuncKey("ESC", { ctrl: true }),
-      new FuncKey("ESC", { shift: true }),
-      new FuncKey("ESC", { ctrl: true, shift: true }),
-      new FuncKey("3", { ctrl: true }),
-    ];
-  }
-  if (text === "\x1b\x1b") {
-    return [
-      new FuncKey("ESC", { alt: true }),
-      new FuncKey("ESC", { alt: true, shift: true }),
-      new FuncKey("ESC", { ctrl: true, alt: true }),
-    ];
-  }
-
-  if (text === "\x7f") {
-    return [
-      new FuncKey("BACKSPACE"),
-      new FuncKey("BACKSPACE", { shift: true }),
-    ];
-  }
-  if (text === "\x08") {
-    return [
-      new FuncKey("BACKSPACE", { ctrl: true }),
-      new FuncKey("BACKSPACE", { ctrl: true, shift: true }),
-    ];
-  }
-  if (text === "\x1b\x7f") {
-    return [
-      new FuncKey("BACKSPACE", { alt: true }),
-      new FuncKey("BACKSPACE", { alt: true, shift: true }),
-    ];
-  }
-  if (text === "\x1b\x08") {
-    return [new FuncKey("BACKSPACE", { ctrl: true, alt: true })];
-  }
-
-  if (text === "\x09") {
-    return [
-      new FuncKey("TAB"),
-      new FuncKey("TAB", { ctrl: true }),
-    ];
-  }
-  if (text === "\x1b\x09") {
-    return [
-      new FuncKey("TAB", { alt: true }),
-      new FuncKey("TAB", { ctrl: true, alt: true }),
-    ];
-  }
-  if (text === "\x1b[Z") {
-    return [
-      new FuncKey("TAB", { shift: true }),
-      new FuncKey("TAB", { ctrl: true, shift: true }),
-    ];
-  }
-  if (text === "\x1b\x1b[Z") {
-    return [new FuncKey("TAB", { alt: true, shift: true })];
-  }
-
-  if (text === "\x20") {
-    return [
-      new CharKey(" "),
-      new CharKey(" ", { shift: true }),
-    ];
-  }
-  if (text === "\x00") {
-    return [
-      new CharKey(" ", { ctrl: true }),
-      new CharKey(" ", { ctrl: true, shift: true }),
-      new CharKey("2", { ctrl: true }),
-    ];
-  }
-  if (text === "\x1b\x20") {
-    return [
-      new CharKey(" ", { alt: true }),
-      new CharKey(" ", { alt: true, shift: true }),
-    ];
-  }
-  if (text === "\x1b\x00") {
-    return [new CharKey(" ", { ctrl: true, alt: true })];
+  const mapped = map.get(text);
+  if (mapped) {
+    return mapped;
   }
 
   if (buf.length === 2 && buf[0] === 0x1b) {
     return [new CharKey(decoder.decode(buf.subarray(1)), { alt: true })];
-  }
-
-  if (text === "\x1f") {
-    return [new CharKey("/", { ctrl: true })];
-  }
-
-  if (text === "0") {
-    return [
-      new CharKey("0"),
-      new CharKey("0", { ctrl: true }),
-    ];
-  }
-
-  if (text === "1") {
-    return [
-      new CharKey("1"),
-      new CharKey("1", { ctrl: true }),
-    ];
   }
 
   /*
