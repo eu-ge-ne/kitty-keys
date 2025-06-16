93c21aa121c991c510ba679b0b8c4660401b5fd8
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-16 12:20:26 -0400

    refactor: parse

--- src/parse.ts
+++ src/parse.ts
@@ -2,11 +2,11 @@ import { CharKey, FuncKey, type Key } from "./key.ts";
 import { map } from "./map.ts";
 
 // CSI number ; modifier ~
-const legacy_csi_re = /(?<number>\d+)(;(?<modifier>.*))?~/s;
+const csi_re = /(?<number>\d+)(;(?<modifier>.*))?~/s;
 // CSI 1 ; modifier {ABCDEFHPQS}
-const legacy_csi1_re = /(1;(?<modifier>.*))?(?<key>[ABCDEFHPQS])/s;
+const csi1_re = /(1;(?<modifier>.*))?(?<key>[ABCDEFHPQS])/s;
 // SS3 {ABCDEFHPQRS}
-const legacy_ss3_re = /(?<key>[ABCDEFHPQRS])/s;
+const ss3_re = /(?<key>[ABCDEFHPQRS])/s;
 
 const decoder = new TextDecoder();
 
@@ -22,19 +22,19 @@ export function parse(buf: Uint8Array): Key[] {
   }
 
   if (buf.length === 2 && buf[0] === 0x1b) {
-    return [new CharKey(decoder.decode(buf.subarray(1)), { alt: true })];
+    return [new CharKey(text.slice(1), { alt: true })];
   }
 
   if (text.startsWith("\x1b[")) {
-    const csi = decoder.decode(buf.subarray(2));
+    const csi = text.slice(2);
 
-    const match0 = csi.match(legacy_csi_re);
+    const match0 = csi.match(csi_re);
     if (match0?.groups) {
       const { number, modifier } = match0.groups;
       return [FuncKey.parse(number!, modifier)];
     }
 
-    const match1 = csi.match(legacy_csi1_re);
+    const match1 = csi.match(csi1_re);
     if (match1?.groups) {
       const { modifier, key } = match1.groups;
       return [FuncKey.parse(key!, modifier)];
@@ -42,9 +42,9 @@ export function parse(buf: Uint8Array): Key[] {
   }
 
   if (text.startsWith("\x1bO")) {
-    const ss3 = decoder.decode(buf.subarray(2));
+    const ss3 = text.slice(2);
 
-    const match = ss3.match(legacy_ss3_re);
+    const match = ss3.match(ss3_re);
     if (match?.groups) {
       const { key } = match.groups;
       return [FuncKey.parse(key!)];
