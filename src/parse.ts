11d7b6ba12ab322a43a4375c24196764ed64dec2
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-15 22:52:30 -0400

    fix

--- src/parse.ts
+++ src/parse.ts
@@ -12,122 +12,70 @@ const decoder = new TextDecoder();
 /**
  * Parse key event from bytes
  */
-export function parse(buf: Uint8Array): Key[] {
+export function parse(buf: Uint8Array): Key | undefined {
   if (buf.length === 0) {
-    return [];
+    return;
   }
 
   if (buf[0] === 0xd) {
-    return [
-      new FuncKey("ENTER"),
-      new FuncKey("ENTER", { ctrl: true }),
-      new FuncKey("ENTER", { shift: true }),
-      new FuncKey("ENTER", { ctrl: true, shift: true }),
-    ];
+    return new FuncKey("ENTER", { ctrl: true, shift: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0xd) {
-    return [
-      new FuncKey("ENTER", { alt: true }),
-      new FuncKey("ENTER", { alt: true, shift: true }),
-      new FuncKey("ENTER", { ctrl: true, alt: true }),
-    ];
+    return new FuncKey("ENTER", { ctrl: true, alt: true, shift: true });
   }
 
   if (buf[0] === 0x7f) {
-    return [
-      new FuncKey("BACKSPACE"),
-      new FuncKey("BACKSPACE", { shift: true }),
-    ];
+    return new FuncKey("BACKSPACE", { shift: true });
   }
   if (buf[0] === 0x8) {
-    return [
-      new FuncKey("BACKSPACE", { ctrl: true }),
-      new FuncKey("BACKSPACE", { ctrl: true, shift: true }),
-    ];
+    return new FuncKey("BACKSPACE", { ctrl: true, shift: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x7f) {
-    return [
-      new FuncKey("BACKSPACE", { alt: true }),
-      new FuncKey("BACKSPACE", { alt: true, shift: true }),
-    ];
+    return new FuncKey("BACKSPACE", { alt: true, shift: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x8) {
-    return [
-      new FuncKey("BACKSPACE", { ctrl: true, alt: true }),
-    ];
+    return new FuncKey("BACKSPACE", { ctrl: true, alt: true });
   }
 
   if (buf[0] === 0x9) {
-    return [
-      new FuncKey("TAB"),
-      new FuncKey("TAB", { ctrl: true }),
-    ];
+    return new FuncKey("TAB", { ctrl: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x9) {
-    return [
-      new FuncKey("TAB", { alt: true }),
-      new FuncKey("TAB", { ctrl: true, alt: true }),
-    ];
+    return new FuncKey("TAB", { ctrl: true, alt: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x1b && buf[2] === 0x5b) {
     if (decoder.decode(buf.subarray(3)) === "Z") {
-      return [
-        new FuncKey("TAB", { alt: true, shift: true }),
-      ];
+      return new FuncKey("TAB", { alt: true, shift: true });
     }
   }
 
   if (buf[0] === 0x1b) {
     if (buf[1] === 0x1b) {
-      return [
-        new FuncKey("ESC", { alt: true }),
-        new FuncKey("ESC", { alt: true, shift: true }),
-        new FuncKey("ESC", { ctrl: true, alt: true }),
-      ];
+      return new FuncKey("ESC", { ctrl: true, alt: true, shift: true });
     } else if (typeof buf[1] === "undefined") {
-      return [
-        new FuncKey("ESC"),
-        new FuncKey("ESC", { ctrl: true }),
-        new FuncKey("ESC", { shift: true }),
-        new FuncKey("ESC", { ctrl: true, shift: true }),
-      ];
+      return new FuncKey("ESC", { ctrl: true, shift: true });
     }
   }
 
   if (buf[0] === 0x20) {
-    return [
-      new CharKey(" "),
-      new CharKey(" ", { shift: true }),
-    ];
+    return new CharKey(" ", { shift: true });
   }
   if (buf[0] === 0x0) {
-    return [
-      new CharKey(" ", { ctrl: true }),
-      new CharKey(" ", { ctrl: true, shift: true }),
-    ];
+    return new CharKey(" ", { ctrl: true, shift: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x20) {
-    return [
-      new CharKey(" ", { alt: true }),
-      new CharKey(" ", { alt: true, shift: true }),
-    ];
+    return new CharKey(" ", { alt: true, shift: true });
   }
   if (buf[0] === 0x1b && buf[1] === 0x0) {
-    return [
-      new CharKey(" ", { ctrl: true, alt: true }),
-    ];
+    return new CharKey(" ", { ctrl: true, alt: true });
   }
 
   if (buf[0] === 31) {
-    return [
-      new CharKey("/", { ctrl: true }),
-    ];
+    return new CharKey("/", { ctrl: true });
   }
 
   if (buf[0] === 48) {
-    return [
-      new CharKey("0", { ctrl: true }),
-    ];
+    return new CharKey("0", { ctrl: true });
   }
 
   if (buf[0] === 0x1b) {
@@ -135,22 +83,19 @@ export function parse(buf: Uint8Array): Key[] {
       const csi = decoder.decode(buf.subarray(2));
 
       if (csi === "Z") {
-        return [
-          new FuncKey("TAB", { shift: true }),
-          new FuncKey("TAB", { ctrl: true, shift: true }),
-        ];
+        return new FuncKey("TAB", { ctrl: true, shift: true });
       }
 
       const match = csi.match(legacy_csi_re);
       if (match?.groups) {
         const { number, modifier } = match.groups;
-        return [FuncKey.parse(number!, modifier)];
+        return FuncKey.parse(number!, modifier);
       }
 
       const match1 = csi.match(legacy_csi1_re);
       if (match1?.groups) {
         const { modifier, key } = match1.groups;
-        return [FuncKey.parse(key!, modifier)];
+        return FuncKey.parse(key!, modifier);
       }
     }
 
@@ -160,18 +105,18 @@ export function parse(buf: Uint8Array): Key[] {
       const match = ss3.match(legacy_ss3_re);
       if (match?.groups) {
         const { key } = match.groups;
-        return [FuncKey.parse(key!)];
+        return FuncKey.parse(key!);
       }
     }
 
-    return [new CharKey(decoder.decode(buf.subarray(1)), { alt: true })];
+    return new CharKey(decoder.decode(buf.subarray(1)), { alt: true });
   }
 
   /*
   if (buf[0]! < 0x20 || buf[0]! === 0x7f) {
-    return [FuncKey.parse(decoder.decode(buf))];
+    return FuncKey.parse(decoder.decode(buf));
   }
   */
 
-  return [new CharKey(decoder.decode(buf))];
+  return new CharKey(decoder.decode(buf));
 }
--- test/legacy.test.ts
+++ test/legacy.test.ts
@@ -5,7 +5,7 @@ import { CharKey, FuncKey, type Key } from "../src/key.ts";
 
 const encoder = new TextEncoder();
 
-function eq(actual: string | number[], expected: Key[]): void {
+function eq(actual: string | number[], expected: Key): void {
   if (typeof actual === "string") {
     assertEquals(parse(encoder.encode(actual)), expected);
   } else {
@@ -14,156 +14,85 @@ function eq(actual: string | number[], expected: Key[]): void {
 }
 
 Deno.test("Characters", () => {
-  eq("a", [new CharKey("a")]);
-  eq("A", [new CharKey("A")]);
-  eq("0", [new CharKey("0")]);
-  eq("$", [new CharKey("$")]);
+  eq("a", new CharKey("a"));
+  eq("A", new CharKey("A"));
+  eq("0", new CharKey("0", { ctrl: true }));
+  eq("$", new CharKey("$"));
 });
 
 Deno.test("Func keys", () => {
-  eq("\x1b[2~", [new FuncKey("INSERT")]);
-  eq("\x1b[3~", [new FuncKey("DELETE")]);
-  eq("\x1b[5~", [new FuncKey("PAGE_UP")]);
-  eq("\x1b[6~", [new FuncKey("PAGE_DOWN")]);
+  eq("\x1b[2~", new FuncKey("INSERT"));
+  eq("\x1b[3~", new FuncKey("DELETE"));
+  eq("\x1b[5~", new FuncKey("PAGE_UP"));
+  eq("\x1b[6~", new FuncKey("PAGE_DOWN"));
 
-  eq("\x1b[A", [new FuncKey("UP")]);
-  eq("\x1bOA", [new FuncKey("UP")]);
-  eq("\x1b[B", [new FuncKey("DOWN")]);
-  eq("\x1bOB", [new FuncKey("DOWN")]);
-  eq("\x1b[C", [new FuncKey("RIGHT")]);
-  eq("\x1bOC", [new FuncKey("RIGHT")]);
-  eq("\x1b[D", [new FuncKey("LEFT")]);
-  eq("\x1bOD", [new FuncKey("LEFT")]);
-  eq("\x1b[H", [new FuncKey("HOME")]);
-  eq("\x1bOH", [new FuncKey("HOME")]);
-  eq("\x1b[F", [new FuncKey("END")]);
-  eq("\x1bOF", [new FuncKey("END")]);
+  eq("\x1b[A", new FuncKey("UP"));
+  eq("\x1bOA", new FuncKey("UP"));
+  eq("\x1b[B", new FuncKey("DOWN"));
+  eq("\x1bOB", new FuncKey("DOWN"));
+  eq("\x1b[C", new FuncKey("RIGHT"));
+  eq("\x1bOC", new FuncKey("RIGHT"));
+  eq("\x1b[D", new FuncKey("LEFT"));
+  eq("\x1bOD", new FuncKey("LEFT"));
+  eq("\x1b[H", new FuncKey("HOME"));
+  eq("\x1bOH", new FuncKey("HOME"));
+  eq("\x1b[F", new FuncKey("END"));
+  eq("\x1bOF", new FuncKey("END"));
 
-  eq("\x1bOP", [new FuncKey("F1")]);
-  eq("\x1bOQ", [new FuncKey("F2")]);
-  eq("\x1bOR", [new FuncKey("F3")]);
-  eq("\x1bOS", [new FuncKey("F4")]);
-  eq("\x1b[15~", [new FuncKey("F5")]);
-  eq("\x1b[17~", [new FuncKey("F6")]);
-  eq("\x1b[18~", [new FuncKey("F7")]);
-  eq("\x1b[19~", [new FuncKey("F8")]);
-  eq("\x1b[20~", [new FuncKey("F9")]);
-  eq("\x1b[21~", [new FuncKey("F10")]);
-  eq("\x1b[23~", [new FuncKey("F11")]);
-  eq("\x1b[24~", [new FuncKey("F12")]);
+  eq("\x1bOP", new FuncKey("F1"));
+  eq("\x1bOQ", new FuncKey("F2"));
+  eq("\x1bOR", new FuncKey("F3"));
+  eq("\x1bOS", new FuncKey("F4"));
+  eq("\x1b[15~", new FuncKey("F5"));
+  eq("\x1b[17~", new FuncKey("F6"));
+  eq("\x1b[18~", new FuncKey("F7"));
+  eq("\x1b[19~", new FuncKey("F8"));
+  eq("\x1b[20~", new FuncKey("F9"));
+  eq("\x1b[21~", new FuncKey("F10"));
+  eq("\x1b[23~", new FuncKey("F11"));
+  eq("\x1b[24~", new FuncKey("F12"));
 
-  eq("\x1b[29~", [new FuncKey("MENU")]);
+  eq("\x1b[29~", new FuncKey("MENU"));
 });
 
 Deno.test("ENTER", () => {
-  eq([0xd], [
-    new FuncKey("ENTER"),
-    new FuncKey("ENTER", { ctrl: true }),
-    new FuncKey("ENTER", { shift: true }),
-    new FuncKey("ENTER", { ctrl: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0xd], [
-    new FuncKey("ENTER", { alt: true }),
-    new FuncKey("ENTER", { alt: true, shift: true }),
-    new FuncKey("ENTER", { ctrl: true, alt: true }),
-  ]);
+  eq([0xd], new FuncKey("ENTER", { ctrl: true, shift: true }));
+  eq([0x1b, 0xd], new FuncKey("ENTER", { ctrl: true, alt: true, shift: true }));
 });
 
 Deno.test("ESC", () => {
-  eq([0x1b], [
-    new FuncKey("ESC"),
-    new FuncKey("ESC", { ctrl: true }),
-    new FuncKey("ESC", { shift: true }),
-    new FuncKey("ESC", { ctrl: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0x1b], [
-    new FuncKey("ESC", { alt: true }),
-    new FuncKey("ESC", { alt: true, shift: true }),
-    new FuncKey("ESC", { ctrl: true, alt: true }),
-  ]);
+  eq([0x1b], new FuncKey("ESC", { ctrl: true, shift: true }));
+  eq([0x1b, 0x1b], new FuncKey("ESC", { ctrl: true, alt: true, shift: true }));
 });
 
 Deno.test("BACKSPACE", () => {
-  eq([0x7f], [
-    new FuncKey("BACKSPACE"),
-    new FuncKey("BACKSPACE", { shift: true }),
-  ]);
-
-  eq([0x8], [
-    new FuncKey("BACKSPACE", { ctrl: true }),
-    new FuncKey("BACKSPACE", { ctrl: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0x7f], [
-    new FuncKey("BACKSPACE", { alt: true }),
-    new FuncKey("BACKSPACE", { alt: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0x8], [
-    new FuncKey("BACKSPACE", { ctrl: true, alt: true }),
-  ]);
+  eq([0x7f], new FuncKey("BACKSPACE", { shift: true }));
+  eq([0x8], new FuncKey("BACKSPACE", { ctrl: true, shift: true }));
+  eq([0x1b, 0x7f], new FuncKey("BACKSPACE", { alt: true, shift: true }));
+  eq([0x1b, 0x8], new FuncKey("BACKSPACE", { ctrl: true, alt: true }));
 });
 
 Deno.test("TAB", () => {
-  eq([0x9], [
-    new FuncKey("TAB"),
-    new FuncKey("TAB", { ctrl: true }),
-  ]);
-
-  eq([0x1b, 0x9], [
-    new FuncKey("TAB", { alt: true }),
-    new FuncKey("TAB", { ctrl: true, alt: true }),
-  ]);
-
-  eq("\x1b[Z", [
-    new FuncKey("TAB", { shift: true }),
-    new FuncKey("TAB", { ctrl: true, shift: true }),
-  ]);
-
-  eq("\x1b\x1b[Z", [
-    new FuncKey("TAB", { alt: true, shift: true }),
-  ]);
+  eq([0x9], new FuncKey("TAB", { ctrl: true }));
+  eq([0x1b, 0x9], new FuncKey("TAB", { ctrl: true, alt: true }));
+  eq("\x1b[Z", new FuncKey("TAB", { ctrl: true, shift: true }));
+  eq("\x1b\x1b[Z", new FuncKey("TAB", { alt: true, shift: true }));
 });
 
 Deno.test("SPACE", () => {
-  eq([0x20], [
-    new CharKey(" "),
-    new CharKey(" ", { shift: true }),
-  ]);
-
-  eq([0x0], [
-    new CharKey(" ", { ctrl: true }),
-    new CharKey(" ", { ctrl: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0x20], [
-    new CharKey(" ", { alt: true }),
-    new CharKey(" ", { alt: true, shift: true }),
-  ]);
-
-  eq([0x1b, 0x0], [
-    new CharKey(" ", { ctrl: true, alt: true }),
-  ]);
+  eq([0x20], new CharKey(" ", { shift: true }));
+  eq([0x0], new CharKey(" ", { ctrl: true, shift: true }));
+  eq([0x1b, 0x20], new CharKey(" ", { alt: true, shift: true }));
+  eq([0x1b, 0x0], new CharKey(" ", { ctrl: true, alt: true }));
 });
 
 Deno.test("alt + character", () => {
-  eq("\x1bi", [new CharKey("i", { alt: true })]);
-  eq("\x1bI", [new CharKey("I", { alt: true })]);
+  eq("\x1bi", new CharKey("i", { alt: true }));
+  eq("\x1bI", new CharKey("I", { alt: true }));
 });
 
 Deno.test("ctrl + character", () => {
-  eq([0], [
-    new CharKey(" ", { ctrl: true }),
-    new CharKey(" ", { ctrl: true, shift: true }),
-  ]);
-
-  eq([31], [
-    new CharKey("/", { ctrl: true }),
-  ]);
-
-  eq([48], [
-    new CharKey("0", { ctrl: true }),
-  ]);
+  eq([0], new CharKey(" ", { ctrl: true, shift: true }));
+  eq([31], new CharKey("/", { ctrl: true }));
+  eq([48], new CharKey("0", { ctrl: true }));
 });
