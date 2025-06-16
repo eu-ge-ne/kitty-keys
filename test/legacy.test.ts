87061127fa052656b37088967171143d75c8d38d
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-15 22:28:29 -0400

    fix

--- test/legacy.test.ts
+++ test/legacy.test.ts
@@ -148,11 +148,12 @@ Deno.test("SPACE", () => {
   ]);
 });
 
-/*
 Deno.test("alt + character", () => {
-  eq("\x1bi", new CharKey("i", { alt: true }));
+  eq("\x1bi", [new CharKey("i", { alt: true })]);
+  eq("\x1bI", [new CharKey("I", { alt: true })]);
 });
 
+/*
 Deno.test("ctrl + character", () => {
   eq([0x0], new CharKey(" ", { ctrl: true }));
 });
