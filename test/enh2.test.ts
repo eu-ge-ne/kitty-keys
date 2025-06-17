c4302e5f1694640d60f9c2dee00f92938d7f7e29
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-16 21:44:39 -0400

    fix

--- test/enh2.test.ts
+++ test/enh2.test.ts
@@ -30,3 +30,20 @@ Deno.test("INSERT", () => {
   is("\x1b[2;1:2~", new_key("INSERT", {}, "repeat"));
   is("\x1b[2;1:3~", new_key("INSERT", {}, "release"));
 });
+
+Deno.test("DELETE", () => {
+  is("\x1b[3;1:1~", new_key("DELETE", {}, "press"));
+  is("\x1b[3;1:2~", new_key("DELETE", {}, "repeat"));
+  is("\x1b[3;1:3~", new_key("DELETE", {}, "release"));
+});
+
+Deno.test("LEFT-RIGHT", () => {
+  is("\x1b[1;1:1D", new_key("LEFT", {}, "press"));
+  is("\x1b[1;1:1C", new_key("RIGHT", {}, "press"));
+
+  is("\x1b[1;1:2D", new_key("LEFT", {}, "repeat"));
+  is("\x1b[1;1:2C", new_key("RIGHT", {}, "repeat"));
+
+  is("\x1b[1;1:3D", new_key("LEFT", {}, "release"));
+  is("\x1b[1;1:3C", new_key("RIGHT", {}, "release"));
+});
