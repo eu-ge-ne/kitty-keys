64e5a227fb238fe2d56a15ff07268f93dd162fe7
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-17 16:48:19 -0400

    fix

--- test/csi-~.test.ts
+++ test/csi-~.test.ts
@@ -12,12 +12,12 @@ Deno.test("F5", () => {
 
   is("\x1b[15~", { key, type: "press" });
 
-  is("\x1b[15;5u", { key, type: "press", ctrl: true });
-  is("\x1b[15;3u", { key, type: "press", alt: true });
-  is("\x1b[15;2u", { key, type: "press", shift: true });
-  is("\x1b[15;65u", { key, type: "press", caps_lock: true });
+  is("\x1b[15;5~", { key, type: "press", ctrl: true });
+  is("\x1b[15;3~", { key, type: "press", alt: true });
+  is("\x1b[15;2~", { key, type: "press", shift: true });
+  is("\x1b[15;65~", { key, type: "press", caps_lock: true });
 
-  is("\x1b[15;1:1u", { key, type: "press" });
-  is("\x1b[15;1:2u", { key, type: "repeat" });
-  is("\x1b[15;1:3u", { key, type: "release" });
+  is("\x1b[15;1:1~", { key, type: "press" });
+  is("\x1b[15;1:2~", { key, type: "repeat" });
+  is("\x1b[15;1:3~", { key, type: "release" });
 });
