e903c3d0c72f56b7aebe1241416cb3e7e7cfa224
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-06-18 14:18:39 -0400

    feat: main.ts (#23)

--- bin/main.ts
+++ bin/main.ts
@@ -1,56 +1,61 @@
 // deno-lint-ignore-file no-console
 import { parse_key } from "../src/mod.ts";
+import { b, s, x, y } from "./fmt.ts";
+import { disable_kitty, enable_kitty, query_kitty } from "./kitty.ts";
 
 Deno.stdin.setRaw(true);
 
 const reader = Deno.stdin.readable.getReader();
-const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-const CSI = "\x1B\x5B";
+enable_kitty(1 + 2 + 4 + 8 + 16);
+query_kitty();
 
-function enable_kitty_protocol(flags: number): void {
-  write(CSI + `=${flags}u`);
-}
+self.onunload = () => {
+  disable_kitty();
+  console.log("\nExit.");
+};
 
-function disable_kitty_protocol(): void {
-  write(CSI + "=0u");
-}
-
-function query_kitty_protocol(): void {
-  write(CSI + "?u");
-}
-
-function write(text: string): void {
-  const buf = encoder.encode(text);
-  let x = 0;
-  while (x < buf.length) {
-    x += Deno.stdout.writeSync(buf.subarray(x));
+for (let i = 0;; i += 1) {
+  if (i % 10 === 0) {
+    console.log(
+      `\n${s("EVENT")}${s("TEXT")}${s("NAME", 15)}${s("KEY")}${s("SHIFT_KEY")}${
+        s("BASE_KEY")
+      }${s("SHIFT")}${s("ALT")}${s("CTRL")}${s("SUPER")}${s("CAPS_LOCK")}${
+        s("NUM_LOCK")
+      }BYTES\n`,
+    );
   }
-}
 
-enable_kitty_protocol(1 /*+ 2*/ + 4 + 8 + 16);
-query_kitty_protocol();
-
-while (true) {
   const { value: buf } = await reader.read();
-  const text = decoder.decode(buf);
 
+  const text = decoder.decode(buf);
   const key = parse_key(buf!);
 
-  if (key) {
-    console.log("\nPARSED:", {
-      buf,
-      text,
-      key,
-    });
-  } else {
-    console.log("\nUNKNOUN:", { buf, text });
+  if (typeof key !== "string") {
+    const {
+      event,
+      text: txt,
+      name,
+      key: k,
+      shift_key,
+      base_key,
+      shift,
+      alt,
+      ctrl,
+      super: sup,
+      caps_lock,
+      num_lock,
+    } = key;
+
+    console.log(
+      `${s(event)}${s(txt)}${s(name, 15)}${x(k)}${x(shift_key)}${x(base_key)}${
+        b(shift)
+      }${b(alt)}${b(ctrl)}${b(sup)}${b(caps_lock)}${b(num_lock)}${y(text)}`,
+    );
   }
 
-  if (text.includes("27")) {
+  if (typeof key === "object" && key.key === "c" && key.ctrl) {
     break;
   }
 }
-
-disable_kitty_protocol();
