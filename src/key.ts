b88bede12ff8e00c1f4535cd332afb7dcf7b69f0
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-06-18 00:17:43 -0400

    docs: jsdoc (#20)

--- src/key.ts
+++ src/key.ts
@@ -1,35 +1,43 @@
 /**
- * Key
+ * Represents key event
+ * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#an-overview}
  */
 export interface Key extends Modifiers {
   /**
-   * Key
+   * Text representation of the `unicode-key-code` field.
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#key-codes}
    */
   key: string;
   /**
-   * Event
+   * Text representation of the `event-type` sub-field.
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#event-types}
    */
   event: "press" | "repeat" | "release";
   /**
-   * Shifted key
+   * Text representation of the `shifted-key-code` field.
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#key-codes}
    */
   shift_key?: string;
   /**
-   * Base layout key
+   * Text representation of the `base-layout-key-code` field.
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#key-codes}
    */
   base_key?: string;
   /**
-   * Text
+   * Text representation of the `text-as-codepoints` field.
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#text-as-code-points}
    */
   text?: string;
   /**
-   * Name
+   * Name of the functional key
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#functional-key-definitions}
    */
   name?: string;
 }
 
 /**
- * Modifiers
+ * Represents modifier keys
+ * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#modifiers}
  */
 export interface Modifiers {
   /**
