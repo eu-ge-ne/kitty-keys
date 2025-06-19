a90d92931427f49f4c6c89db705c7d89ca64d771
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-06-19 16:18:07 -0400

    fix: parse (#28)

--- test/utils.ts
+++ test/utils.ts
@@ -6,7 +6,3 @@ const encoder = new TextEncoder();
 export function is(actual: string, expected: Key): void {
   assertEquals(parse_key(encoder.encode(actual)), expected);
 }
-
-export function is_text(actual: string, expected: string): void {
-  assertEquals(parse_key(encoder.encode(actual)), expected);
-}
