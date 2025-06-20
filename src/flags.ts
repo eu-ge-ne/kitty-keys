9c6a4d9826a216ddee7f86a3c81b7d1bc89e5554
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-19 21:00:05 -0400

    fix

--- src/flags.ts
+++ src/flags.ts
@@ -7,25 +7,25 @@ export interface Flags {
 
   /**
    * 0b10 (2) Report event types
-   * https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-events
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-events}
    */
   events?: boolean;
 
   /**
    * 0b100 (4) Report alternate keys
-   * https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-alternates
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-alternates}
    */
-  alternate_keys?: boolean;
+  alternates?: boolean;
 
   /**
    * 0b1000 (8) Report all keys as escape codes
-   * https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-all-keys
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-all-keys}
    */
-  escape_codes?: boolean;
+  all?: boolean;
 
   /**
    * 0b10000 (16) Report associated text
-   * https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-text
+   * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-text}
    */
   text?: boolean;
 }
