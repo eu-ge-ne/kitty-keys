cbb3a418e16d4c4ec51cc7f51e31d3012564b86b
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-06-20 15:00:22 -0400

    fix

--- README.md
+++ README.md
@@ -115,6 +115,38 @@ while (true) {
 
 ### `Flags`
 
+The progressive enhancement flags. See
+<https://sw.kovidgoyal.net/kitty/keyboard-protocol/#id5>.
+
+```ts ignore
+interface Flags {
+  /**
+   * 0b1 (1) Disambiguate escape codes.
+   */
+  disambiguate?: boolean;
+
+  /**
+   * 0b10 (2) Report event types.
+   */
+  events?: boolean;
+
+  /**
+   * 0b100 (4) Report alternate keys.
+   */
+  alternates?: boolean;
+
+  /**
+   * 0b1000 (8) Report all keys as escape codes.
+   */
+  all_keys?: boolean;
+
+  /**
+   * 0b10000 (16) Report associated text.
+   */
+  text?: boolean;
+}
+```
+
 ## Links
 
 - <https://sw.kovidgoyal.net/kitty/keyboard-protocol>
--- src/flags.ts
+++ src/flags.ts
@@ -1,36 +1,36 @@
 import { decoder, encoder } from "./codec.ts";
 
 /**
- * The progressive enhancement flags
+ * The progressive enhancement flags.
  * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#id5}
  */
 export interface Flags {
   /**
-   * 0b1 (1) Disambiguate escape codes
+   * 0b1 (1) Disambiguate escape codes.
    * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#disambiguate-escape-codes}
    */
   disambiguate?: boolean;
 
   /**
-   * 0b10 (2) Report event types
+   * 0b10 (2) Report event types.
    * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-events}
    */
   events?: boolean;
 
   /**
-   * 0b100 (4) Report alternate keys
+   * 0b100 (4) Report alternate keys.
    * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-alternates}
    */
   alternates?: boolean;
 
   /**
-   * 0b1000 (8) Report all keys as escape codes
+   * 0b1000 (8) Report all keys as escape codes.
    * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-all-keys}
    */
   all_keys?: boolean;
 
   /**
-   * 0b10000 (16) Report associated text
+   * 0b10000 (16) Report associated text.
    * @see {@link https://sw.kovidgoyal.net/kitty/keyboard-protocol/#report-text}
    */
   text?: boolean;
